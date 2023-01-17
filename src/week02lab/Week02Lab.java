a82cc9227ca264c20a51321cb4015ed2cd88053b
Tejasvi <Tejasvi@LAPTOP-SD1N1RG1>
2023-01-17 09:31:31 -0700

    New Project Creation - Week02Lab Create a new Java Application for HelloWorld.

---
+++ src/week02lab/Week02Lab.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week02lab;
+
+/**
+ *
+ * @author Tejasvi
+ */
+public class Week02Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!!!");
+        System.out.println("This is getting to know GitHub more Part 2 for week 2 lab...");
+    }
+    
+}
